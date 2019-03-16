5998657c6b81345d5e6f06d375db87feeeda32fb
noor <shaikhibrahim7737@gmail.com>
2019-03-16 20:21:10 +0530

    Dlogin Register

--- app/src/main/java/com/example/fooduck/NGOLogin.java
+++ app/src/main/java/com/example/fooduck/NGOLogin.java
@@ -1,14 +1,18 @@
 package com.example.fooduck;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -47,24 +51,29 @@ public class NGOLogin extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+
                 mMail = mEmail.getText().toString();
                 mPass = mPassword.getText().toString();
 
 
                 if(!mMail.isEmpty() && !mPass.isEmpty())
-                {
-                    mAuth.signInWithEmailAndPassword(mMail,mPass).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    mAuth.signInWithEmailAndPassword(mMail,mPass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                         @Override
-                        public void onSuccess(AuthResult authResult) {
+                        public void onComplete(@NonNull Task<AuthResult> task) {
 
+                            if (task.isSuccessful()){
+                                Toast.makeText(NGOLogin.this,"Successfully",Toast.LENGTH_LONG).show();
 
+                            }else {
+                                Toast.makeText(NGOLogin.this,"Not Successfully",Toast.LENGTH_LONG).show();
 
+                            }
 
                         }
                     });
                 }
 
-            }
+
         });
 
     }
