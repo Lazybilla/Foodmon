cb3a76df08b70dccee8f85d6e2666b2ff5970072
noor <shaikhibrahim7737@gmail.com>
2019-03-16 19:16:31 +0530

    Dlogin Register

--- app/src/main/java/com/example/fooduck/DLogin.java
+++ app/src/main/java/com/example/fooduck/DLogin.java
@@ -1,10 +1,12 @@
 package com.example.fooduck;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.AuthResult;
@@ -19,7 +21,8 @@ public class DLogin extends AppCompatActivity {
     private EditText mPassword ;
     private Button  mSignIn;
     private String mMail , mPass ;
-    private FirebaseAuth mAuth ;
+    private FirebaseAuth mAuth;
+    private TextView dregister;
 
 
     @Override
@@ -30,6 +33,17 @@ public class DLogin extends AppCompatActivity {
         mEmail = (EditText)findViewById(R.id.dmail);
         mPassword = (EditText)findViewById(R.id.dpassword);
         mSignIn = (Button)findViewById(R.id.dbutton);
+        dregister = findViewById(R.id.dregister);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        dregister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(DLogin.this,DRegister.class);
+                startActivity(i);
+            }
+        });
 
 
 
@@ -42,17 +56,10 @@ public class DLogin extends AppCompatActivity {
                 mPass = mPassword.getText().toString();
 
 
-                if(!mMail.isEmpty() && !mPass.isEmpty())
-                {
-
-
+                if(!mMail.isEmpty() && !mPass.isEmpty()) {
                     mAuth.signInWithEmailAndPassword(mMail,mPass).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
                         @Override
                         public void onSuccess(AuthResult authResult) {
-
-
-
-
 
 
                         }
