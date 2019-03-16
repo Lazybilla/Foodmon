2ad3fb121d9e7fc2312b958e74a82c71cc88bd1c
lazybilla <lazybillla@gmail.com>
2019-03-16 16:51:16 +0530

    Login page bangaya

--- app/src/main/java/com/example/fooduck/NGOLogin.java
+++ app/src/main/java/com/example/fooduck/NGOLogin.java
@@ -2,15 +2,12 @@ package com.example.fooduck;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class NGOLogin extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
+        setContentView(R.layout.activity_ngologin);
     }
 }
