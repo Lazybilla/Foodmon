0d9ad02997a5ba4946a9f850fa2ba58a887c3661
Ibrahim <shaikhibrahim7737@gmail.com>
2019-03-16 17:25:11 +0530

    ngo register

--- app/src/main/java/com/example/fooduck/MainActivity.java
+++ app/src/main/java/com/example/fooduck/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.fooduck;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -22,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
         ngoopen.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,NGOLogin.class);
+                startActivity(intent);
 
             }
         });
@@ -29,6 +32,10 @@ public class MainActivity extends AppCompatActivity {
         dopen.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this,DLogin.class);
+                startActivity(intent);
+
 
             }
         });
--- app/src/main/java/com/example/fooduck/NGORegister.java
+++ app/src/main/java/com/example/fooduck/NGORegister.java
@@ -3,14 +3,11 @@ package com.example.fooduck;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class DLogin extends AppCompatActivity {
-
+public class NGORegister extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dlogin);
-
-
+        setContentView(R.layout.activity_ngoregister);
     }
 }
