5235f2620855fb361f811850c44c30190df00a83
noor <shaikhibrahim7737@gmail.com>
2019-03-16 21:19:05 +0530

    image upload

--- app/src/main/java/com/example/fooduck/NGORegister.java
+++ app/src/main/java/com/example/fooduck/NGORegister.java
@@ -1,15 +1,27 @@
 package com.example.fooduck;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class NGORegister extends AppCompatActivity {
 
+    private static final int GET_FROM_GALLERY1 = 1234;
+    private static final int GET_FROM_GALLERY2 = 12345;
+    private static final int GET_FROM_GALLERY3 = 12346;
+
     private EditText mNgoName ,mNgoID, mAadharUri,mGovtDoctUri,mLetterUri,mPhoneNumber,mEmail,mPassword,mRepassword ;
-    private Button mGovDoctButton,mAadharButton,mRegister;
+    private Button mGovDoctButton,mAadharButton,mLetterbtn,mRegister;
     private String mName,mID,mPhone,mMail,mPass,mRepass;
 
     @Override
@@ -31,8 +43,9 @@ public class NGORegister extends AppCompatActivity {
         // Button
         mGovDoctButton = (Button)findViewById(R.id.govt_doc_btn);
         mAadharButton = (Button)findViewById(R.id.aadhar_img_btn);
-        mRegister = (Button)findViewById(R.id.register);
+        mLetterbtn = (Button)findViewById(R.id.letter_btn);
 
+        mRegister = (Button)findViewById(R.id.register);
 
 
 
@@ -42,6 +55,7 @@ public class NGORegister extends AppCompatActivity {
         mGovDoctButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                startActivityForResult(new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI), GET_FROM_GALLERY2);
 
             }
         });
@@ -49,6 +63,18 @@ public class NGORegister extends AppCompatActivity {
         mAadharButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                startActivityForResult(new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI), GET_FROM_GALLERY1);
+
+
+            }
+        });
+
+        mLetterbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivityForResult(new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI), GET_FROM_GALLERY3);
 
             }
         });
@@ -66,19 +92,34 @@ public class NGORegister extends AppCompatActivity {
                 mRepass = mRepassword.getText().toString();
 
 
-
-
-
-
-
-
-
-
             }
         });
 
-
-
-
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+        //Detects request codes
+        if(requestCode==GET_FROM_GALLERY1 && resultCode == Activity.RESULT_OK) {
+            Uri selectedImage1 = data.getData();
+            mAadharUri.setText(String.valueOf(selectedImage1));
+
+            }
+            if(requestCode==GET_FROM_GALLERY2 && resultCode == Activity.RESULT_OK) {
+                Uri selectedImage2 = data.getData();
+                mGovtDoctUri.setText(String.valueOf(selectedImage2));
+                }
+
+                if(requestCode==GET_FROM_GALLERY3 && resultCode == Activity.RESULT_OK) {
+                    Uri selectedImage3 = data.getData();
+                    mLetterUri.setText(String.valueOf(selectedImage3));
+                    }
+
+        }
+
+
+
 }
