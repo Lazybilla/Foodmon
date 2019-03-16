510763d1395ac0a1243c2be8f9a50e24a1f6dc3e
lazybilla <lazybillla@gmail.com>
2019-03-16 19:58:48 +0530

    FIREBASE AUTH SOLVED

--- app/src/main/java/com/example/fooduck/MainActivity.java
+++ app/src/main/java/com/example/fooduck/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.firebase.FirebaseApp;
+
 public class MainActivity extends AppCompatActivity {
 
     private Button ngoopen,dopen;
@@ -16,6 +18,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
 
         ngoopen = findViewById(R.id.ngoopen);
         dopen = findViewById(R.id.dopen);
