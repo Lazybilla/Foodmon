d13fd3f2b5897b24e9bd7e45ceb20e302ac406dd
Ibrahim <shaikhibrahim7737@gmail.com>
2019-03-16 16:52:30 +0530

    main activty

--- app/src/main/java/com/example/fooduck/MainActivity.java
+++ app/src/main/java/com/example/fooduck/MainActivity.java
@@ -2,15 +2,37 @@ package com.example.fooduck;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button ngoopen,dopen;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ngoopen = findViewById(R.id.ngoopen);
+        dopen = findViewById(R.id.dopen);
+
+        ngoopen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        dopen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
     }
+
 }
