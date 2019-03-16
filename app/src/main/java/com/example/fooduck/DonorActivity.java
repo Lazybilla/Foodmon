857713f597b6c0e78d53794128bedbf82809c918
lazybilla <lazybillla@gmail.com>
2019-03-17 02:04:17 +0530

    DEFINED

--- app/src/main/java/com/example/fooduck/DonorActivity.java
+++ app/src/main/java/com/example/fooduck/DonorActivity.java
@@ -16,5 +16,8 @@ public class DonorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_donor);
+
+
+
     }
 }
