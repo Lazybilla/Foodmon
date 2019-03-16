2c4860aedf09efd77cd7d485641d0136848d9181
lazybilla <lazybillla@gmail.com>
2019-03-16 17:26:45 +0530

    Login page pura hogaya bachkaney

--- app/src/main/java/com/example/fooduck/DLogin.java
+++ app/src/main/java/com/example/fooduck/DLogin.java
@@ -2,14 +2,52 @@ package com.example.fooduck;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.messaging.FirebaseMessaging;
 
 public class DLogin extends AppCompatActivity {
+
+
+
+    private EditText mEmail ;
+    private EditText mPassword ;
+    private Button  mSignIn;
+    private String mMail , mPass ;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dlogin);
+
+        mEmail = (EditText)findViewById(R.id.dmail);
+        mPassword = (EditText)findViewById(R.id.dpassword);
+        mSignIn = (Button)findViewById(R.id.dbutton);
+
+
+
+        mSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                mMail = mEmail.getText().toString();
+                mPass = mPassword.getText().toString();
+
+
+
+
+
+
+            }
+        });
+
+
+
+
 
 
     }
--- app/src/main/java/com/example/fooduck/NGOLogin.java
+++ app/src/main/java/com/example/fooduck/NGOLogin.java
@@ -2,12 +2,44 @@ package com.example.fooduck;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class NGOLogin extends AppCompatActivity {
+
+
+    private EditText mEmail;
+    private EditText mPassword;
+    private Button mSignIn;
+    private String mMail , mPass ;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ngologin);
+
+
+        mEmail = (EditText)findViewById(R.id.ngomail);
+        mPassword = (EditText)findViewById(R.id.ngopassword);
+        mSignIn = (Button)findViewById(R.id.ngobutton);
+
+
+
+        mSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mMail = mEmail.getText().toString();
+                mPass = mPassword.getText().toString();
+
+
+            }
+        });
+
+
+
     }
 }
